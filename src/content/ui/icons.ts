c481ae49c337d8f080446dfcd6cfee4256a788d8
Dany <89309539+danbenba@users.noreply.github.com>
2026-08-02 17:49:03 +0200

    feat(ui): redraw the icon set on chatgpt geometry with 1.33 rounded strokes

--- src/content/ui/icons.ts
+++ src/content/ui/icons.ts
@@ -1,62 +1,67 @@
-export const logoSvg = `<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M10 12.917V3.75M10 3.75L6.25 7.5M10 3.75L13.75 7.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M3.75 13.75V15C3.75 15.6904 4.30964 16.25 5 16.25H15C15.6904 16.25 16.25 15.6904 16.25 15V13.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
-</svg>`;
+const ICON_ATTRS =
+  'viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" stroke-width="1.33" stroke-linecap="round" stroke-linejoin="round"';
 
-export const exportIconSvg = `<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M11.25 3.125H5.625C4.93464 3.125 4.375 3.68464 4.375 4.375V15.625C4.375 16.3154 4.93464 16.875 5.625 16.875H14.375C15.0654 16.875 15.625 16.3154 15.625 15.625V7.5L11.25 3.125Z" stroke="currentColor" stroke-width="1.4" stroke-linejoin="round"/>
-<path d="M11.25 3.125V7.5H15.625" stroke="currentColor" stroke-width="1.4" stroke-linejoin="round"/>
-<path d="M10 14.375V9.6875M10 9.6875L8.125 11.5625M10 9.6875L11.875 11.5625" stroke="currentColor" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>`;
+function icon(paths: string): string {
+  return `<svg ${ICON_ATTRS}>${paths}</svg>`;
+}
 
-export const closeIconSvg = `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M6 6L18 18M18 6L6 18" stroke="currentColor" stroke-width="1.8" stroke-linecap="round"/>
-</svg>`;
+export const logoSvg = icon(`
+<path d="M11.667 4.167H6.25c-1.151 0-2.083.932-2.083 2.083v7.5c0 1.151.932 2.083 2.083 2.083h7.5c1.151 0 2.083-.932 2.083-2.083V8.333"/>
+<path d="M10.417 9.583 16.25 3.75"/>
+<path d="M12.5 3.75h3.75V7.5"/>
+`);
 
-export const checkIconSvg = `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M4.5 12.5L9.5 17.5L19.5 7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>`;
+export const exportIconSvg = icon(`
+<path d="M16.667 9.583c0 3.107-2.985 5.625-6.667 5.625-.77 0-1.51-.11-2.197-.313L4.583 16.25l.99-2.834c-1.39-1.03-2.24-2.472-2.24-4.062 0-3.107 2.985-5.625 6.667-5.625s6.667 2.518 6.667 5.625Z"/>
+<path d="M10 6.667v4.166"/>
+<path d="M8.125 8.958 10 6.667l1.875 2.291"/>
+`);
 
-export const chevronIconSvg = `<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M5 7.5L10 12.5L15 7.5" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>`;
+export const closeIconSvg = icon(`<path d="M5.417 5.417 14.583 14.583M14.583 5.417 5.417 14.583"/>`);
 
-export const copyIconSvg = `<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="7.25" y="7.25" width="9" height="9" rx="2" stroke="currentColor" stroke-width="1.4"/>
-<path d="M12.75 4.75C12.75 4.05964 12.1904 3.5 11.5 3.5H5C4.30964 3.5 3.75 4.05964 3.75 4.75V11.25C3.75 11.9404 4.30964 12.5 5 12.5" stroke="currentColor" stroke-width="1.4" stroke-linecap="round"/>
-</svg>`;
+export const checkIconSvg = icon(`<path d="M4.167 10.417 8.333 14.583 15.833 5.833"/>`);
 
-export const downloadIconSvg = `<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M10 3.125V12.5M10 12.5L6.5 9M10 12.5L13.5 9" stroke="currentColor" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M3.75 13.75V15C3.75 15.6904 4.30964 16.25 5 16.25H15C15.6904 16.25 16.25 15.6904 16.25 15V13.75" stroke="currentColor" stroke-width="1.4" stroke-linecap="round"/>
-</svg>`;
+export const chevronIconSvg = icon(`<path d="M5.417 7.917 10 12.5l4.583-4.583"/>`);
 
-export const markdownIconSvg = `<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="1.75" y="4.25" width="16.5" height="11.5" rx="2" stroke="currentColor" stroke-width="1.4"/>
-<path d="M4.5 12.5V7.5L7 10L9.5 7.5V12.5" stroke="currentColor" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M13.75 7.5V12.5M13.75 12.5L11.75 10.5M13.75 12.5L15.75 10.5" stroke="currentColor" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>`;
+export const copyIconSvg = icon(`
+<path d="M8.125 7.083h6.25c.575 0 1.042.467 1.042 1.042v6.25c0 .575-.467 1.042-1.042 1.042h-6.25a1.042 1.042 0 0 1-1.042-1.042v-6.25c0-.575.467-1.042 1.042-1.042Z"/>
+<path d="M12.5 5V4.583c0-.575-.467-1.041-1.042-1.041h-5.83c-.576 0-1.045.466-1.045 1.041v5.834c0 .575.467 1.041 1.042 1.041H5.833"/>
+`);
 
-export const htmlIconSvg = `<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M7 6.5L3.5 10L7 13.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M13 6.5L16.5 10L13 13.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M11 4.5L9 15.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
-</svg>`;
+export const downloadIconSvg = icon(`
+<path d="M10 3.75v7.5"/>
+<path d="M7.083 8.75 10 11.667l2.917-2.917"/>
+<path d="M3.75 13.333v.834c0 1.15.933 2.083 2.083 2.083h8.334c1.15 0 2.083-.933 2.083-2.083v-.834"/>
+`);
 
-export const jsonIconSvg = `<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M7.5 3.5C6 3.5 5.5 4.5 5.5 5.75C5.5 7 5.75 8.25 4 8.9V11.1C5.75 11.75 5.5 13 5.5 14.25C5.5 15.5 6 16.5 7.5 16.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
-<path d="M12.5 3.5C14 3.5 14.5 4.5 14.5 5.75C14.5 7 14.25 8.25 16 8.9V11.1C14.25 11.75 14.5 13 14.5 14.25C14.5 15.5 14 16.5 12.5 16.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
-</svg>`;
+export const markdownIconSvg = icon(`
+<path d="M3.75 5.417h12.5c.46 0 .833.373.833.833v7.5c0 .46-.373.833-.833.833H3.75a.833.833 0 0 1-.833-.833v-7.5c0-.46.373-.833.833-.833Z"/>
+<path d="M5.833 12.083V7.917L7.917 10 10 7.917v4.166"/>
+<path d="M13.333 7.917v4.166M11.667 10.417l1.666 1.666L15 10.417"/>
+`);
 
-export const textIconSvg = `<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M4 5.5H16M4 10H16M4 14.5H11" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
-</svg>`;
+export const htmlIconSvg = icon(`
+<path d="M6.667 6.667 3.333 10l3.334 3.333"/>
+<path d="M13.333 6.667 16.667 10l-3.334 3.333"/>
+<path d="M11.25 4.583 8.75 15.417"/>
+`);
 
-export const pdfIconSvg = `<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M11.5 2.75H6C5.30964 2.75 4.75 3.30964 4.75 4V16C4.75 16.6904 5.30964 17.25 6 17.25H14C14.6904 17.25 15.25 16.6904 15.25 16V6.5L11.5 2.75Z" stroke="currentColor" stroke-width="1.4" stroke-linejoin="round"/>
-<path d="M11.5 2.75V6.5H15.25" stroke="currentColor" stroke-width="1.4" stroke-linejoin="round"/>
-<path d="M7.5 13.5H12.5M7.5 10.75H12.5" stroke="currentColor" stroke-width="1.4" stroke-linecap="round"/>
-</svg>`;
+export const jsonIconSvg = icon(`
+<path d="M8.333 3.75c-1.38 0-2.083.7-2.083 2.083 0 1.384.208 2.5-1.667 2.5v3.334c1.875 0 1.667 1.116 1.667 2.5 0 1.383.703 2.083 2.083 2.083"/>
+<path d="M11.667 3.75c1.38 0 2.083.7 2.083 2.083 0 1.384-.208 2.5 1.667 2.5v3.334c-1.875 0-1.667 1.116-1.667 2.5 0 1.383-.703 2.083-2.083 2.083"/>
+`);
+
+export const textIconSvg = icon(`
+<path d="M4.167 5.833h11.666"/>
+<path d="M4.167 10h11.666"/>
+<path d="M4.167 14.167h7.083"/>
+`);
+
+export const pdfIconSvg = icon(`
+<path d="M11.25 2.917H6.25c-1.151 0-2.083.932-2.083 2.083v10c0 1.151.932 2.083 2.083 2.083h7.5c1.151 0 2.083-.932 2.083-2.083V7.5l-4.583-4.583Z"/>
+<path d="M11.25 2.917V7.5h4.583"/>
+<path d="M7.5 11.667h5M7.5 14.167h3.333"/>
+`);
 
 export const formatIcons: Record<string, string> = {
   markdown: markdownIconSvg,
@@ -65,3 +70,8 @@ export const formatIcons: Record<string, string> = {
   text: textIconSvg,
   pdf: pdfIconSvg,
 };
+
+export const spinnerSvg = `<svg class="gptx-spinner-svg" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="10" cy="10" r="7.5" stroke="currentColor" stroke-width="1.33" opacity="0.25"/>
+<path d="M17.5 10a7.5 7.5 0 0 0-7.5-7.5" stroke="currentColor" stroke-width="1.33" stroke-linecap="round"/>
+</svg>`;
